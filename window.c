7f900a5f7b2bd1fab1029ce5379a06c4cc036706
Maarten Joosten <mjoosten@f1r4s4.codam.nl>
2021-12-13 16:07:41 +0100

    Window not created upon wrong file

--- window.c
+++ window.c
@@ -6,7 +6,7 @@
 /*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 16:01:09 by mjoosten          #+#    #+#             */
-/*   Updated: 2021/12/13 15:17:31 by mjoosten         ###   ########.fr       */
+/*   Updated: 2021/12/13 16:07:23 by mjoosten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ t_window	*ft_create_window(char *file)
 	window = malloc(sizeof(*window));
 	if (!window)
 		ft_error(0);
+	window->map = ft_create_map(file);
+	window->size = ft_mapsize(window->map);
 	window->mlx = mlx_init();
 	if (!window->mlx)
 		ft_error(0);
 	window->win = mlx_new_window(window->mlx, DISPLAY_X, DISPLAY_Y, "FdF");
 	if (!window->win)
 		ft_error(0);
-	window->map = ft_create_map(file);
-	window->size = ft_mapsize(window->map);
 	window->gradient = 20 / window->size->y;
 	window->mouse = 0;
 	window->x = 0;
